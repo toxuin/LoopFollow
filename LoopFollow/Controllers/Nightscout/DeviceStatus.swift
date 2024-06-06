a33980f4d9c070a89d9a814f61994d91891e5954
toxuin <otstoypuksa@ya.ru>
2024-06-06 13:05:48 -0600

    Check if pump data from NS is not empty

--- LoopFollow/Controllers/Nightscout/DeviceStatus.swift
+++ LoopFollow/Controllers/Nightscout/DeviceStatus.swift
@@ -100,19 +100,21 @@ extension MainViewController {
                                    .withDashSeparatorInDate,
                                    .withColonSeparatorInTime]
         if let lastPumpRecord = lastDeviceStatus?["pump"] as! [String : AnyObject]? {
-            if let lastPumpTime = formatter.date(from: (lastPumpRecord["clock"] as! String))?.timeIntervalSince1970  {
-                if let reservoirData = lastPumpRecord["reservoir"] as? Double {
-                    latestPumpVolume = reservoirData
-                    infoManager.updateInfoData(type: .pump, value: String(format: "%.0f", reservoirData) + "U")
-                } else {
-                    latestPumpVolume = 50.0
-                    infoManager.updateInfoData(type: .pump, value: "50+U")
-                }
-
-                if let uploader = lastDeviceStatus?["uploader"] as? [String: AnyObject],
-                   let upbat = uploader["battery"] as? Double {
-                    infoManager.updateInfoData(type: .battery, value: String(format: "%.0f", upbat) + "%")
-                    UserDefaultsRepository.deviceBatteryLevel.value = upbat
+            if let clockStr = lastPumpRecord["clock"] as? String {
+                if let lastPumpTime = formatter.date(from: (lastPumpRecord["clock"] as! String))?.timeIntervalSince1970  {
+                    if let reservoirData = lastPumpRecord["reservoir"] as? Double {
+                        latestPumpVolume = reservoirData
+                        infoManager.updateInfoData(type: .pump, value: String(format: "%.0f", reservoirData) + "U")
+                    } else {
+                        latestPumpVolume = 50.0
+                        infoManager.updateInfoData(type: .pump, value: "50+U")
+                    }
+                    
+                    if let uploader = lastDeviceStatus?["uploader"] as? [String: AnyObject],
+                       let upbat = uploader["battery"] as? Double {
+                        infoManager.updateInfoData(type: .battery, value: String(format: "%.0f", upbat) + "%")
+                        UserDefaultsRepository.deviceBatteryLevel.value = upbat
+                    }
                 }
             }
         }
